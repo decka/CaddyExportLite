57585bbd950c2211537e8044aa6d557ae13b3e32
Daniel Ryding <decka.dee@gmail.com>
2012-11-26 17:19:41 +1100

    Added client code. Simple test at this stage recieving the pure string from the Hub.

---
+++ CaddyExportLite.Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SignalR.Client.Hubs;
+
+namespace CaddyExportLite.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Connect to the service
+            var hubConnection = new HubConnection("http://localhost:10790/");
+
+            // Create a proxy to the chat service
+            var caddyExportHub = hubConnection.CreateProxy("CaddyExportHub");
+
+            // Print the message when it comes in
+            caddyExportHub.On("addMessage", message => Console.WriteLine(message));
+
+            // Start the connection
+            hubConnection.Start().Wait();
+
+            caddyExportHub.Invoke("SetClientGUID", "226d7253-012a-457c-b98c-f3e82e0d7bf3");
+
+            string line = null;
+            while ((line = Console.ReadLine()) != null)
+            {
+            }
+        }
+    }
+}
