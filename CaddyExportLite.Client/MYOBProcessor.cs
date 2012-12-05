6e1d226cb1e16e54dbd403851a2987dec2ff7cab
Daniel Ryding <decka.dee@gmail.com>
2012-12-05 21:44:07 +1100

    Created early mockup of MYOB client processor.

---
+++ CaddyExportLite.Client/MYOBProcessor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace CaddyExportLite.Client
+{
+    public class MYOBProcessor
+    {
+        private IDbConnection databaseConnection;
+
+        public void TryConnectToDatasource()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TryInsertData(string DataToInsert)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+ 
