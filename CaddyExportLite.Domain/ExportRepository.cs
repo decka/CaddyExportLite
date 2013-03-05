32cd7cd3b57da10bde5945d64cf1bc701fd896ec
Daniel Ryding <decka.dee@gmail.com>
2013-03-05 16:16:23 +1100

    Moved database access / repository to new project.

---
+++ CaddyExportLite.Domain/ExportRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using Dapper;
+
+namespace CaddyExportLite.Domain
+{
+    public class ExportRepository : IExportRepository
+    {
+        private readonly IDbConnection Connection;
+
+        public ExportRepository(IDbConnection connection)
+        {
+            this.Connection = connection;
+            this.Connection.Open();
+        }
+        public IEnumerable<dynamic> FetchExportListing()
+        {
+            return Connection.Query("dbo.ExportTask_Listing", new { @HideCompleted = 0 }, commandType: CommandType.StoredProcedure);
+        }
+        public IEnumerable<string> FetchExportStringsForID(int ID)
+        {
+            return Connection.Query<string>("dbo.Export_MYOB_Purchases_Misc", new { @ID = ID }, commandType: CommandType.StoredProcedure);
+        }
+        public void MarkExportAsComplete(int exportID, string result)
+        {
+            Connection.Query("dbo.ExportTask_Result", new { @ExportID = exportID, @Result = result }, commandType: CommandType.StoredProcedure);
+        }
+    }
+}
